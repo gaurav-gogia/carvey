07c74d0a82204ae0b81ea1e081f57e379a51303b
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2023-11-28 22:29:21 +0530

    update algo - speed up - simplify

--- carvey.go
+++ carvey.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base32"
+	"bytes"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -24,14 +23,7 @@ func main() {
 		handle(err)
 	}
 
-	fpath := os.Args[1]
-	finfo, err := os.Stat(fpath)
-	handle(err)
-
-	src, err := os.Open(fpath)
-	handle(err)
-
-	err = carvePng(DST, src, finfo.Size())
+	err = carvePng(DST, os.Args[1])
 	handle(err)
 }
 
@@ -42,110 +34,46 @@ func handle(err error) {
 	}
 }
 
-func carvePng(dstPath string, read *os.File, size int64) error {
-	buff := make([]byte, 1)
-	var counter int8
-	var carved []byte
+var (
+	HEADER = []byte{0x89, 0x50, 0x4e, 0x47, 0x0D, 0x0A, 0x1A, 0x0A}
+	FOOTER = []byte{0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82}
+)
 
-	if _, err := read.Seek(0, 0); err != nil {
-		return err
-	}
-
-	for i := int64(0); i < size; i++ {
-		if _, err := read.Read(buff); err != nil {
-			return err
-		}
-
-		switch counter {
-		case 0:
-			if buff[0] == 0x89 {
-				carved = append(carved, buff[0])
-				counter++
-			} else {
-				carved = nil
-				counter = 0
-			}
-		case 1:
-			if buff[0] == 0x50 {
-				carved = append(carved, buff[0])
-				counter++
-			} else {
-				carved = nil
-				counter = 0
-			}
-		case 2:
-			if buff[0] == 0x4e {
-				carved = append(carved, buff[0])
-				counter++
-			} else {
-				carved = nil
-				counter = 0
-			}
-		case 3:
-			if buff[0] == 0x47 {
-				carved = append(carved, buff[0])
-				counter++
-			} else {
-				carved = nil
-				counter = 0
-			}
-		case 4:
-			if buff[0] == 0xae {
-				carved = append(carved, buff[0])
-				counter++
-			} else {
-				carved = append(carved, buff[0])
-			}
-		case 5:
-			if buff[0] == 0x42 {
-				carved = append(carved, buff[0])
-				counter++
-			} else {
-				carved = append(carved, buff[0])
-				counter--
-			}
-		case 6:
-			if buff[0] == 0x60 {
-				carved = append(carved, buff[0])
-				counter++
-			} else {
-				carved = append(carved, buff[0])
-				counter -= 2
-			}
-		case 7:
-			if buff[0] == 0x82 {
-				carved = append(carved, buff[0])
-				if err := writecarved(dstPath, "png", &carved); err != nil {
-					return err
-				}
-				carved = nil
-				counter = 0
-			} else {
-				carved = append(carved, buff[0])
-				counter -= 3
-			}
-		}
-
-	}
-
-	return nil
-}
-
-func writecarved(dstPath, ext string, data *[]byte) error {
-	name := filepath.Join(dstPath, getimgname(10)+"."+ext)
-	if err := os.WriteFile(name, *data, 0644); err != nil {
-		return err
-	}
-	_, fname := filepath.Split(name)
-	fmt.Printf("\rImage file found: %s", fname)
-	return nil
-}
-
-func getimgname(length int) string {
-	randomBytes := make([]byte, 32)
-	_, err := rand.Read(randomBytes)
+func carvePng(dstPath string, src string) error {
+	data, err := os.ReadFile(src)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	return base32.StdEncoding.EncodeToString(randomBytes)[:length]
+
+	var count int
+	start := bytes.Index(data, HEADER)
+	end := bytes.Index(data, FOOTER)
+
+	for start != -1 || end != -1 {
+		end = end + len(FOOTER)
+		if start < end {
+			count++
+			fdata := data[start : end-1]
+			err = writeCarved(count, dstPath, fdata)
+			if err != nil {
+				return err
+			}
+		}
+
+		data = data[end:]
+		start = bytes.Index(data, HEADER)
+		end = bytes.Index(data, FOOTER)
+	}
+
+	return nil
+}
+
+func writeCarved(count int, dstPath string, data []byte) error {
+	fname := fmt.Sprintf("IMAGE_%d.png", count)
+	fpath := filepath.Join(dstPath, fname)
+	if err := os.WriteFile(fpath, data, 0644); err != nil {
+		return err
+	}
+	fmt.Printf("\rCarved: %s", fname)
+	return nil
 }
